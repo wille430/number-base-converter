8157b1473edbd894062b1b6d872eb6ea41fd32f3
wille430 <41762983+wille430@users.noreply.github.com>
2022-03-26 13:21:49 +0100

    feat: conversion between base a to base b

--- main.py
+++ main.py
@@ -1,54 +1,72 @@
-
-
-from asyncio.windows_events import NULL
 import math
-import numbers
 
+alphabet = [chr(i) for i in range(ord('a'), ord('z')+1)]
 
-def number_in_array_of_base_n(base_n: int, remaining: int, numbers: list = [], last_exponent: int = NULL) -> list:
+def num_to_char(num):
+    if (num <= 9):
+        return num
+    else:
+        return alphabet[num-10]
+
+def char_to_num(char):
+    if (alphabet.count(char)):
+        return alphabet.index(char) + 10
+    else:
+        # TODO: catch when char is not a number
+        return int(char)
+        
+
+def from_base_10_to_n(base_n: int, remaining: int, numbers: list = [], last_exponent: int = None) -> list:
+
+    if ((last_exponent == 0 and last_exponent) or remaining == 0):
+        return ''.join(list(map(lambda x: '0' if x == None else str(x), numbers)))
+
+    # hittar den största exponenten
     exponent = math.floor(math.log(remaining) / math.log(base_n))
 
+    # hittar den största faktorn framför exponenten
     factor = math.floor(remaining / base_n**exponent)
-    numbers.append(factor)
 
-    for x in range(1, (exponent-last_exponent)-2):
-        if (x == 1):
-            continue
-        numbers.append(0)
+    # Om numbers är tom tilldela den en lista med samma antal element som exponentens värde (index 0 kommer motsvara a**n, index 1: a**(n-1) osv)
+    if (len(numbers) == 0):
+        numbers = [None] * (exponent + 1)
+        
+    # lägger till faktorn i listan
+    numbers[len(numbers) - exponent - 1] = num_to_char(factor)
 
+    # beräknar resterande summa
     remaining -= factor * base_n**exponent
 
-    print("Factor", factor, f" of {base_n}^{exponent}")
-    print("Remaining:", remaining)
-
-    if (remaining == 0):
-        for x in range(0, exponent):
-            numbers.append(0)
-
-    if(remaining > 0 and exponent > 0):
-        return number_in_array_of_base_n(base_n, remaining, numbers, exponent)
+    if(exponent > 0):
+        return from_base_10_to_n(base_n, remaining, numbers, exponent)
     else:
-        return numbers
+        return ''.join(list(map(lambda x: '0' if x == None else str(x), numbers)))
 
+def from_base_n_to_base_10(base_n, number) -> list:
+    # skapa en lista med varje siffra i numret
+    nums = [char_to_num(x) for x in str(number)]
 
-def base_10_to_base_n(base_n: int, number: int):
-    # hittar n i ekvationen: base_n^n >= log number
-    print(f"Input: {number}")
-    print(f"Base: 10")
+    sum = 0
 
-    if (base_n > 10):
-        raise 'Maximum supported base is 10'
+    i = 0
+    for num in nums:
+        exponent = (len(nums) - 1)-i
+        sum += num * base_n**exponent
 
-    number_in_base_n = ''.join(str(x) for x in number_in_array_of_base_n(base_n, number))
+        i += 1
 
-    print(f"Output: {number_in_base_n}")
-    print(f"Base: {base_n}")
+    return sum
 
 
 def main():
-    base = int(input('Base:'))
-    number = int(input('Number:'))
-    base_10_to_base_n(base, number)
+    base_a = int(input('From base:'))
+    number = input('Number to convert:')
+    base_b = int(input('To base:'))
+
+    num_base_10 = from_base_n_to_base_10(base_a, number)
+    num_base_b = from_base_10_to_n(base_b, num_base_10) if base_b != 10 else num_base_10
+
+    print("Result: ", num_base_b)
 
 
 if __name__ == "__main__":
